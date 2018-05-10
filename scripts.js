6f296e54842056dd77892b2b597d5bc64fb6f27f
Mark Rosenberg <mrk.rosenberg@gmail.com>
2018-05-10 12:58:54 -0600

    adds volume, playback rate controls

--- scripts.js
+++ scripts.js
@@ -43,6 +43,10 @@ function skip() {
     video.currentTime += parseFloat(this.dataset.skip);
 };
 
+function handleRangeUpdate() {
+    console.log(this.value);
+};
+
 
 // hook up event listeners
 video.addEventListener('click', togglePlay);
@@ -52,3 +56,6 @@ video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
